8e3dccb40232e88e2d5ab0759f2a4efa3efb507f
abhijithc <abhijithc366@gmail.com>
2026-02-17 15:20:42 +0530

    Added fetch api for ml-service

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,20 @@
 import express from 'express';
-
+import logger from './middleware/logger.js';
 const app = express();
+const PYTHON_API = 'localhost'
+
+app.use(express.json());
+app.use(logger);
+
+
+app.get('api/predict_price', async (req, res, next) => {
+    const result = await fetch(`${PYTHON_API}/predict_price`);
+    res.json(result); 
+});
+
+app.get('api/max_contribution', async (req, res, next)=>{
+    const result = await fetch(`${PYTHON_API}/max_contribution`);
+    res.json(result);
+});
 
 export default app; 
