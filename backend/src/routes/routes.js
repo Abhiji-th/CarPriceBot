c21913c178c4bdfe972c17b80623b4b525626f67
abhijithc <abhijithc366@gmail.com>
2026-02-18 10:22:50 +0530

    Updated routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import {getPredictPrice, getMaxContribution} from '../controllers/controller.js'
 const router = Router();
 
-router.post('/api/predict_price', getPredictPrice);
-router.post('/api/max_contribution', getMaxContribution);
+router.post('/predict_price', getPredictPrice);
+router.post('/max_contribution', getMaxContribution);
 
 export default router;
