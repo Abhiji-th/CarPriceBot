fcdb3b1093ccfab3d045fed1e3dc3cea217b08be
abhiji-th <abhijithc366@gmail.com>
2026-03-10 21:51:33 +0530

    Added docker compose

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -501,7 +501,7 @@ class ActionPredictCarPrice(Action):
         seats = tracker.get_slot('seats')
 
         # Construct the API URL
-        api_url = f"http://127.0.0.1:8000/api/predict_price"
+        api_url = f"http://backend:8000/api/predict_price"
 
         payload = {
             "brand": brand,
@@ -582,7 +582,7 @@ class ActionMaxContribution(Action):
 
         # Construct the API URL with the car details
         # Construct the API URL
-        api_url = f"http://127.0.0.1:8000/api/max_contribution"
+        api_url = f"http://backend:8000/api/max_contribution"
 
         payload = {
             "brand": brand,
--- frontend/src/api/chatApi.jsx
+++ frontend/src/api/chatApi.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { useEffect } from "react";
 
-const RASA_API = "http://localhost:5005/webhooks/rest/webhook"
+const RASA_API = "http://rasaservice:5005/webhooks/rest/webhook";
 
 export const sendMessage = (message) => {
-    return axios.post(RASA_API, {
-        sender: 'user',
-        message: message,
-    });
+  return axios.post(RASA_API, {
+    sender: "user",
+    message: message,
+  });
 };
