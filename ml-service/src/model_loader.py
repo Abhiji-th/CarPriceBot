1b971fa14259e51ea60df17d1bfe69b2720f8670
abhiji-th <abhijithc366@gmail.com>
2026-03-10 18:44:14 +0530

    Added dockerfiles for chatbot and backend:

--- ml-service/src/model_loader.py
+++ ml-service/src/model_loader.py
@@ -1,4 +1,8 @@
+from pathlib import Path
 import joblib
 
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_PATH = BASE_DIR.parent / "models" / "rf_model_8.pkl"
+
 def load_model():
-    return joblib.load('../models/rf_model_8.pkl')
+    return joblib.load(MODEL_PATH)
